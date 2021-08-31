577ada3520ed3c8e4576060cc2d17fee348829db
Hoorooroo <madsong940811@gmail.com>
2021-09-01 00:53:03 +0900

    move arrow button

--- js/main.js
+++ js/main.js
@@ -22,7 +22,60 @@ backToTop.addEventListener('click', moveBackToTop);
 
 /*-----------------------------------------------------*/
 
-const transformPrev = () => {};
+const transformNext = (event) => {
+	const slideNext = event.target;
+	const slidePrev = slideNext.previousElementSibling;
+
+	const classList = slideNext.parentElement.parentElement.nextElementSibling;
+	let activeLi = classList.getAttribute('data-position');
+	const liList = classList.getElementsByTagName('li');
+
+	if(Number(activeLi) < 0) {
+		activeLi = Number(activeLi) + 260;
+
+		slidePrev.style.color = '#2f3059';
+		slidePrev.classList.add('slide-prev-hovver');
+		slidePrev.addEventListener('click', transformPrev);
+
+		if(Number(activeLi) === 0) {
+			slideNext.style.color = '#cfd8dc';
+			slideNext.classList.remove('slide-next-hover');
+			slideNext.removeEventListener('click', transformNext);
+		}
+	}
+
+	classList.style.transition = 'transform 1s';
+	classList.style.transform = 'translateX(' + String(activeLi) + 'px)';
+	classList.setAttribute('data-position', activeLi);
+}
+
+
+const transformPrev = (event) => {
+	const slidePrev = event.target;
+	const slideNext = slidePrev.nextElementSibling;
+
+	const classList = slidePrev.parentElement.parentElement.nextElementSibling;
+	let activeLi = classList.getAttribute('data-position');
+	const liList = classList.getElementsByTagName('li');
+
+	if(classList.clientWidth < (liList.length * 260 + Number(activeLi))) {
+		activeLi = Number(activeLi) - 260;
+
+		if(classList.clientWidth > (liList.length * 260 + Number(activeLi))) {
+			slidePrev.style.color = '#cfd8dc';
+			slidePrev.classList.remove('slide-prev-hover');
+			slidePrev.removeEventListener('click', transformPrev);
+		}
+
+		slideNext.style.color = '#2f3059';
+		slideNext.classList.add('slide-next-hover');
+		slideNext.addEventListener('click', transformNext);
+	}
+
+	classList.style.transition = 'transform 1s';
+	classList.style.transform = 'translateX(' + String(activeLi) + 'px)';
+	classList.setAttribute('data-position', activeLi);
+};
 
 const slidePrevList = document.getElementsByClassName('slide-prev');
 
