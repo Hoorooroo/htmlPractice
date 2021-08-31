0be00f27c3465125502e4d7c87d8597bd026444a
Hoorooroo <madsong940811@gmail.com>
2021-08-31 23:26:02 +0900

    arrow button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const backToTop = document.getElementById('backtotop');
 
-function checkScroll() {
+const checkScroll = () => {
 
 	let pageYOffset = window.pageYOffset;
 
@@ -11,7 +11,7 @@ function checkScroll() {
 	}
 }
 
-function moveBackToTop() {
+const moveBackToTop = () => {
 	if(window.pageYOffset > 0) {
 		window.scrollTo({top: 0, behavior: "smooth"})
 	}
@@ -19,3 +19,23 @@ function moveBackToTop() {
 
 window.addEventListener('scroll', checkScroll);
 backToTop.addEventListener('click', moveBackToTop);
+
+/*-----------------------------------------------------*/
+
+const transformPrev = () => {};
+
+const slidePrevList = document.getElementsByClassName('slide-prev');
+
+for (let i = 0; i < slidePrevList.length; i++) {
+	let classList = slidePrevList[i].parentElement.parentElement.nextElementSibling;
+	let liList = classList.getElementsByTagName('li');
+
+	if(classList.clientWidth < ((liList.length) * 260) ) {
+		slidePrevList[i].classList.add('slide-prev-hover');
+		slidePrevList[i].addEventListener('click', transformPrev);
+	} else {
+		const arrowContainer = slidePrevList[i].parentElement;
+		arrowContainer.removeChild(slidePrevList[i].nextElementSibling);
+		arrowContainer.removeChild(slidePrevList[i]);
+	}
+}
